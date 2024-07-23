8b10c87ac4e02ff37b9fe9471c5ab7937aa064d3
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-07-23 18:55:47 -0300

    closes #4 - Made the script to show the students in the table

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
 $("#inputTel").mask("(00) 00000-0000");
+
+var students = [
+    {id: 1, name: "Pedro Antonio", email: "pedro@abutua.com", phone: "(15) 9999-9999", course: 2, shift: 2}
+];
+
+var course = ["Java", "Angular", "HTML/CSS"];
+var shift = ["Manhã", "Tarde", "Noite"];
+
+function addStudentToTable(student){
+    var table = document.getElementById("studentsTable");
+    var newRow = table.insertRow();
+
+    var Node = document.createTextNode(student.id);
+    newRow.insertCell().appendChild(Node);
+
+    Node = document.createTextNode(student.name);
+    newRow.insertCell().appendChild(Node);
+
+    Node = document.createTextNode(student.email);
+    var cell = newRow.insertCell();
+    cell.className = "d-none d-md-table-cell";
+    cell.appendChild(Node);
+
+    Node = document.createTextNode(student.phone);
+    cell = newRow.insertCell();
+    cell.className = "d-none d-md-table-cell";
+    cell.appendChild(Node);
+
+    Node = document.createTextNode(course[student.course - 1]);
+    cell = newRow.insertCell();
+    cell.className = "d-none d-md-table-cell";
+    cell.appendChild(Node);
+
+    Node = document.createTextNode(shift[student.shift - 1]);
+    cell = newRow.insertCell();
+    cell.className = "d-none d-md-table-cell";
+    cell.appendChild(Node);
+}
+
+function showStudents(){
+    for(student of students){
+        addStudentToTable(student);
+    }
+}
+
+showStudents();
